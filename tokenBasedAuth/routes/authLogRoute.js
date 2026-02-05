65896274927085b1290497ae3d150bca2ceb31a9
Sujay Barman <bsujay619@gmail.com>
2026-02-05 17:49:50 +0530

    fix: replace bcrypt with bcryptjs for production compatibility

--- tokenBasedAuth/routes/authLogRoute.js
+++ tokenBasedAuth/routes/authLogRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwtUtils = require("../utils/jwt");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { User } = require("../models/user");
 const router = express.Router();
 
--- tokenBasedAuth/routes/authRegRoutes.js
+++ tokenBasedAuth/routes/authRegRoutes.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const { User } = require('../models/user');
 const jwtUtils = require('../utils/jwt');
